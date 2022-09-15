c37dc333c8a7dea51680f15a3fd4f6ed41f93d26
UP210163 <112640935+UP210163@users.noreply.github.com>
2022-09-15 09:28:47 -0500

    Add files via upload

---
+++ U1/01_Hola.cpp
@@ -0,0 +1,24 @@
+/*
+Date: 12/09/2022
+Author: Jeannelyn Avila Jimenez
+Email: up210163@alumnos.upa.edu.mx
+Description: Mi primer programa en c++
+Last Modification:
+*/
+
+//-------PREPROCESSOR DIRECTIVES-------//
+
+#include<iostream>//Library input/output
+
+//------MAIN FUNCTION------//
+
+int main (){
+    //Prints on screen a text
+    std::cout<<"Hello World! Thats my first program";
+
+
+
+
+//return 0 means that the programm ends succesfully
+return 0;
+}

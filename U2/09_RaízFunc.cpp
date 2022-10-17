4fbb04ee9a902f55840d1b81847b87bbfe726d75
UP210356 <up210356@alumnos.upa.edu.mx>
2022-10-17 10:34:50 -0500

    Raíz

--- U2/09_RaízFunc.cpp
+++ U2/09_RaízFunc.cpp
@@ -5,30 +5,76 @@
 */
 
 #include <iostream>
-#include <math.h>
-
+#include <cmath>
+#include <iomanip>
 using namespace std;
+
+
 float resolverEcuacion(float valor){
-    return pow(valor,2)+3*(valor)+8;
+   
+    return pow(valor,2)-(3*valor)-12;
 }
 
- int main(){
-    float a,b;
-    float c=0;
-    float ya=resolverEcuacion(a);
-    float yb=resolverEcuacion(b);
-    float yc=resolverEcuacion(c);
-
-    cout<<"ingresa el valor de a:  ";
-    cin>>a;
-    cout<<"ingresa el valor de b:  ";
-    cin>>b;
-
-   c=(a+b)/2;
-
-   
 
 
+int main(){
+    int cont=1;
+    float a,b,c,ya,yb,yc;
+    cout << "Give me the value of a: ";
+    cin >> a;
+    cout << "Give me the value of b: ";
+    cin >> b;
+    c = (a+b)/2;
+    ya= resolverEcuacion(a);
+    yb= resolverEcuacion(b);
+    yc= resolverEcuacion(c);
+     for (int i=0; i<137; i++)
+    {
+        cout<< "~";
+    }
+    cout<< "\n";
+    if ((yb>0 && ya<0) || (ya>0 && yb<0)){
+    cout<< "|\t NO.\t|\t a\t|\t b\t|\t c \t|\t f(a) \t\t|\t f(b) \t\t|\t f(c) \t\t| \n";
+    for (int i=0; i<137; i++)
+    {
+        cout<< "~";
+    }
+    cout<< "\n";
+    cout<< "| \t" << cont << "\t|\t" << fixed << setprecision(3) << a << "\t|\t" << b << "\t|\t" << c << "\t|\t" << ya << "\t\t|\t" << yb << "\t\t|\t" << yc << "\t\t|\n"; //First data before anything
+    for (int i=0; i<137; i++)
+    {
+        cout<< "~";
+    }
+    cout<< "\n";
 
+        while (yc>=0.01 || yc<=-0.01)
+        {
+            if ((yc>0 && ya<0) || (ya>0 && yc<0))
+            {
+                b = c;
+            }
+            else
+            {
+                a = c;
+            }
+            
+            c = (a+b)/2;
+            ya = resolverEcuacion(a);
+            yb = resolverEcuacion(b);
+            yc = resolverEcuacion(c);
+            cont++;
+            cout<< "| \t" << cont<< "\t|\t" << fixed << setprecision(3) << a << "\t|\t" << b << "\t|\t" << c << "\t|\t" << ya << "\t\t|\t" << yb << "\t\t|\t" << yc << "\t\t|\n";
+            for (int i = 0; i <137; i++)
+            {
+                cout<< "~";
+            }
+            cout<< "\n";
+        }
+    cout<< "--The root is approximately "<< setprecision(1) <<c<<"--"<<endl;
+    }
+    else
+    {
+        cout << "~There's no root between the numbers~"<<endl;
+    }
     return 0;
- }
+}

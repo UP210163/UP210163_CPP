5b277c3b9f83cb4d2b51c51640a4bcb6111daeaa
UP210356 <up210356@alumnos.upa.edu.mx>
2022-11-04 09:46:26 -0600

    Gato

--- U3/04_Gato.cpp
+++ U3/04_Gato.cpp
@@ -7,36 +7,104 @@ Description: EJercicio "Juego del Gato"
 #include <iostream>
 
 using namespace std;
-void tablero();
-int main()
-{
-    char planogato[6][11];
-    for (int row = 0; row < 6; row++)
-    {
-        for (int  col = 0; col < 11; col++)
-        {
-        
-            if (col == 3 || col == 7){
-            planogato[row][col]='|';
-            }else if (row==1 || row == 3){
-                planogato[row][col]='_';
+
+
+void hacerestructura();
+int seleccionarJugada();
+void convertirJugada(int jugada);
+bool comprobarCasillaOcupada(int jugada);
+
+
+char estructuraGato[3][3];
+int row = 0;
+int col = 0;
+int jugada;
+
+
+int main(){
+   estructuraGato[2][1] = 'i';
+    estructuraGato[2][2] = 'o';
+
+    hacerestructura();
+    seleccionarJugada();
+    
+    
+    
+    return 0;
+}
+
+void hacerestructura(){
+    for(int fila = 0; fila < 6; fila++){
+        for(int columna = 0; columna < 3; columna++){
+
+            if(fila < 5 && fila%2 == 1){
+                cout << "___";
+            }else{
+                if(fila < 5){
+                    cout << " " << estructuraGato[row][col] << " ";
+                    col++;
+                }else{
+                    cout << "   ";
+                }
             }
-            else {
-               planogato[row][col]=' '; 
+
+            if(columna < 2){
+                cout << "|";
             }
-            
         }
-    }
-    for (int row = 0; row < 6; row++)
-    {
-        for (int  col = 0; col < 11; col++)
-        {
-            cout << planogato[row][col];
+        col = 0;
+        if(fila % 2 == 0){
+            row++;
         }
         cout << endl;
     }
-    return 0;
 }
-void tablero(){
 
+int seleccionarJugada(){
+    int jugada=0; 
+    
+    }
+
+
+
+
+
+void convertirJugada(int jugada){
+    if(jugada == 1){
+        row = 0;
+        col = 0;
+    }else if(jugada == 2){
+        row = 0;
+        col = 1;
+    }else if(jugada == 3){
+        row = 0;
+        col = 2;
+    }else if(jugada == 4){
+        row = 1;
+        col = 0;
+    }else if(jugada == 5){
+        row = 1;
+        col = 1;
+    }else if(jugada == 6){
+        row = 1;
+        col = 2;
+    }else if(jugada == 7){
+        row = 2;
+        col = 0;
+    }else if(jugada == 8){
+        row = 2;
+        col = 1;
+    }else if(jugada == 9){
+        row = 2;
+        col = 2;
+    }
+}
+
+
+bool comprobarCasillaOcupada(int jugada){
+    if(estructuraGato[row][col] == 'X' || estructuraGato[row][col] == 'O'){
+        return true;
+    }else{
+        return false;
+    }
 }

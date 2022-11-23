836e2726ad56111c6cf75558198f493277fd9dcf
UP210163 <112640935+UP210163@users.noreply.github.com>
2022-11-23 09:15:46 -0600

    Update 04_Gato.cpp

--- U3/04_Gato.cpp
+++ U3/04_Gato.cpp
@@ -3,7 +3,6 @@ Date: 3/10/2022
 Author: Jeannelyn Avila Jimenez.
 Description: EJercicio "Juego del Gato"
 */
-
 #include <iostream>
 
 using namespace std;
@@ -15,6 +14,7 @@ int seleccionarJugada();
 bool comprobarCasillaOcupada(int jugada);
 void colocarJugada(int jugada);
 void modoDeJuego(int);
+//-----Recursos utilizados por la PC en el juego
 int mejorJugada(char);
 void colocarJugadaPCenX(int);
 void colocarJugadaPCenO(int);
@@ -24,13 +24,15 @@ int turnoPC();
 bool comprobarCasillaOcupadaPC(int);
 bool revisarGanadorJugador(int jugada);
 bool revisarGanadorPC(int);
-
+//------------------------------------------------
 int turnoJugador = 0;
+
+//-----Asignación de las variables constantes(variables que no cambian en todo el programa) 
+//para el modo de juego contra la computadora
 
 const char PC = 'O';
 const char JUGADOR = 'X';
-const string TABLERO = "Real";
-const string TABLEROPC = "Imaginario";
+//-------------------------------------------------
 
 
 int main(){
@@ -38,46 +40,49 @@ int main(){
     int modoDeJuego;
     bool casillaOcupada = true;
     bool ganador = false;
+
+
     cout<< "-TIC TAC TOE-\n \n";
     tableroJugador();
     cout<< "Selecciona el modo de juego: \n|--------------------------|\n|1. Jugador 1 VS Jugador 2 | \n|2. Jugador 1 VS PC        |      \n|Opcion: ";
     cin>> modoDeJuego;
 
-    if(modoDeJuego == 1){
+    if(modoDeJuego == 1){//Utilizada para indicar la parte del codigo y parametros a utilizar dado el modo de juego
         do
         {
             jugada = seleccionarJugada();
             casillaOcupada = comprobarCasillaOcupada(jugada);
+            //Condicional que indica la repeticion del codigo por casilla ocupada
             if(casillaOcupada == true){
                 do
                 {
                     cout<<"Casilla ocupada, haz otro intento";
                     break;
                 } while (casillaOcupada = true);
-            }
+            }//-----------------------------------
             else
-            {
-                system("clear");
-                colocarJugada(jugada);
-                tableroJugador();
-                turnoJugador++;
+            {//Condicional que indica la continuacion del codigo porque no hay casilla ocupada
+                system("clear");//Limpia toda la terminal 
+                colocarJugada(jugada);//Una vez que ya se comprobo la casilla libre se coloca la jugada
+                tableroJugador();//Rehace el tablero ya que se borro anteriormente
+                turnoJugador++;//Aumenta el turno de jugador que sera utilizado en mas funciones(variable declarada en la linea 30)
             }
-        ganador=revisarGanadorJugador(ganador);
-        } while (ganador == false && turnoJugador < 9);
+        ganador=revisarGanadorJugador(ganador);//Revisa que haya un ganador al termino de cada colocacion de jugada
+        } while (ganador == false && turnoJugador < 9);//Condicional que se repetira mientras no haya un ganador y el turno sea menor a nueve
     if (turnoJugador < 9)
         {
             if (turnoJugador % 2 == 0)
             {
-                cout << "Gano el jugador 2" <<endl;
+                cout << "Gano el jugador 2" <<endl;//Indica que el jugador 2 gana dado que sus turnos siempre seran pares y porque sus variables fueron definidas en colocar jugada 301
             }
             else
             {
-                cout << "Gano el jugador 1" <<endl;
+                cout << "Gano el jugador 1" <<endl;//Indica que le jugador 1 gana ya que sus turnos son impares
             }
         }
         else
         {
-            cout << "---Empate---" <<endl;
+            cout << "---Empate---" <<endl;//si los turnos pasan de 9 y no hay ganador la variable indica empate
         }
     }
     else if (modoDeJuego == 2)
@@ -136,13 +141,13 @@ int turnoPC()
     int jugada;
     bool jugadaUtilizada = false;
     jugada = mejorJugada(PC);
-    if (jugada != -1)
+    if (jugada != -1)//El -1 indica que si la computadora no esta en peligro de perder ni tampoco con posibilidad de ganar debe poner una jugada random
     {
         return jugada;
     }
 
     jugada= mejorJugada(JUGADOR);
-    if (jugada != -1)
+    if (jugada != -1)//el -1 indica que si el jugador no lleva ventaja se colocara una random en el tablero
     {
         return jugada;
     }
@@ -231,7 +236,7 @@ int mejorJugada(char entradaJugador)
     if (entradaJugador == 'X')
     {
         do
-        {
+        {//Este apartado se encarga de simular las jugadas del jugador en el plano imaginario comprobando si hay peligro de que este gane
             jugadaPC++;
             jugadaUsada = comprobarCasillaOcupadaPC(jugadaPC);
             if (jugadaUsada == false)
@@ -244,7 +249,7 @@ int mejorJugada(char entradaJugador)
     }
     else
     {
-        do
+        do//Este apartado simula la jugada de la pc comprobando las jugadas ganadoras
         {
             jugadaPC++;
             jugadaUsada = comprobarCasillaOcupadaPC(jugadaPC);
@@ -279,7 +284,7 @@ int seleccionarJugada(){
 
 bool comprobarCasillaOcupada(int jugada)
 {
-    int row = jugada / 10; 
+    int row = jugada / 10; 
     int col = jugada - 1;
     if (areaDeJuego[row][col] == 'X' || areaDeJuego[row][col] == 'O')
     {
@@ -347,13 +352,13 @@ void tableroJugador(){
             
             else if (row!=5 &&(col ==9 || col== 5 || col==1))
             {
-                coordenadasMatriz[row][col]= areaDeJuego[f1][c1];  
+                coordenadasMatriz[row][col]= areaDeJuego[f1][c1];  
                 c1++;
                 if (c1 == 3)
                 {
                     c1 = 0;
                     f1++;
-                } 
+                } 
             }
             
             else{
